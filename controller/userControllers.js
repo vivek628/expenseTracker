8973fecbf98c80b61950e5f9bd31684e08d32591
vivek628 <siloriv2@gmail.com>
2024-07-22 07:54:19 +0530

    login page

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const User= require('../model/user')
 const path=require('path')
+const { where } = require('sequelize')
 exports.singup=(req,res,next)=>{
     const Method=req.method
     if(Method=='GET')
@@ -19,7 +20,33 @@ exports.singup=(req,res,next)=>{
             console.log(e)
          })
     }
-   
+
 
    
+}
+exports.login=(req,res,next)=>{
+    if(req.method=='GET')
+    {
+        res.sendFile(path.join(__dirname,'../public/views/','login.html'))
+    }
+    else{
+        const email= req.body.email;
+        const password=req.body.password;
+        User.findAll({where:{email:email}}).then((data)=>{
+            if(data.length==0)
+            {
+                res.status(404).send('email does not exist')
+            }
+            else{
+                if(data[0].password != password)
+                {
+                    res.status(404).send('password is wrong')
+                }
+                else{
+                    res.send('Login successful');
+                }
+            }
+        }).catch((e)=>console.log(e))
+    }
+  
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,7 @@
 const express= require('express')
 const route= express.Router()
 const userController=require('../controller/userControllers')
+route.use('/login',userController.login)
 route.use('/',userController.singup)
+
 module.exports=route
