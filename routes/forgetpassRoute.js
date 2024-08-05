77b5ceb398582beff24f2d9e30c382e082091213
vivek628 <siloriv2@gmail.com>
2024-08-05 11:50:17 +0530

    passwordreset

--- routes/forgetpassRoute.js
+++ routes/forgetpassRoute.js
@@ -1,6 +1,9 @@
 const express= require('express')
 const route= express.Router()
 const forgetpasswordcontroller= require('../controller/forgetpassController')
+route.get('/getresetpassword',forgetpasswordcontroller.getresetPasswordUrl)
 route.get('/forgetpassword',forgetpasswordcontroller.forget)
 route.post('/resetpassword',forgetpasswordcontroller.resetpass)
+route.post('/postresetpassword',forgetpasswordcontroller.postresetpass)
+
 module.exports= route
